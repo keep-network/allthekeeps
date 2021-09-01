5ef2ffcca2882a6f771f89b4f58aa8dcbbdc348b
Dmitry <dymitr.paremski@keep.network>
2021-09-01 16:25:50 +0200

    Moving to keep everything in the main branch

--- install-repositories.sh
+++ install-repositories.sh
@@ -8,9 +8,3 @@ LOG_END='\n\e[0m' # new line + reset color
 printf "${LOG_START}Initializing submodules...${LOG_END}"
 
 git submodule update --init --recursive --remote --rebase --force
-
-printf "${LOG_START}Switching to 'for-allthekeeps-on-ropsten' branch...${LOG_END}"
-
-cd ./keep-subgraph
-git checkout for-allthekeeps-on-ropsten
-git pull
